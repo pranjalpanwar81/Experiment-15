89a787fb167e4bbeae26439c5eeb72b91ae66146
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-10-08 21:45:05 +0530

    Create Exp15C.cpp

---
+++ Exp15C.cpp
@@ -0,0 +1,30 @@
+// NAME - PRANJAL PANWAR 
+// PRN - 23070123164 
+// EXPERIMENT - 15(C) 
+
+#include<iostream>
+using namespace std;      
+
+// Creating function. 
+void reverse(char *str)
+{
+    if(*str)  // Base Condition 
+    {
+        reverse(str+1);  // Recursion 
+        cout<<("%c",*str);
+    }
+}
+
+int main() 
+{
+    char a[50];
+    cout<<"Enter a string: ";
+    cin>>a;
+    reverse(a);  // Function calling 
+    return 0; 
+}       
+
+// Output 
+// 
+// Enter a string: Symbiosis
+// sisoibmyS
